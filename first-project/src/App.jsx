1532a14466098a73a445e89f05eac4d0e2339f83
Son Seung Min <sonsmmm@naver.com>
2025-06-23 14:12:04 +0900

    JSX기초

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+function Basic() {
+  const name = "John";
+  const age = 25;
+  const isAdmin = true;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Name: {name}</p>
+      <p>Age next year: {age + 1}</p>
+      <p>{name + " 's profile"}</p> {/* nmae 오타 수정 */}
+      <p>{`${name} is ${age} years old`}</p>
+      <p>Admin status: {String(isAdmin)}</p>
     </>
-  )
+  );
 }
 
-export default App
+function App() {
+  return (
+    <>
+      <h1>JSX</h1>
+      <Basic />
+    </>
+  );
+}
+
+export default App;
