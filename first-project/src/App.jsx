fff26f442701cadf9cd3fbca7d4abba0495a6de1
Son Seung Min <sonsmmm@gmail.com>
2025-06-24 16:44:52 +0900

    useEffect

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,33 +1,25 @@
 import './App.css'
 import { useState } from 'react'
-import TempInput from './TempInput'
-import UnitSelector from './UnitSelector'
+import Timer from './Timer'
 
 const App = () => {
-  const [temperature, setTemperature] = useState("")
-  const [unit, setUnit] = useState("Celsius")
-
-  const convertedTemp = unit === "Celsius"
-    ? (temperature * 9/5 + 32).toFixed(1)
-    : ((temperature - 32) * 5/9).toFixed(1)
+  const [showTimer, setShowTimer]
+   = useState(false)
 
   return (
-    <div>
-      <h2>Temperature Converter</h2>
-      <p>
-        Converted: {temperature ? convertedTemp : "--"} 
-        {unit === "Celsius" ? "°F" : "°C"}
-      </p>
-      <TempInput
-        value={temperature}
-        unit={unit}
-        onChange={setTemperature}
-      />
-      <UnitSelector
-        unit={unit}
-        onUnitChange={setUnit}
-      />
-    </div>
+    <>
+      <label>
+        <input
+          type="checkbox"
+          checked={showTimer}
+          onChange={
+            (e) => setShowTimer(
+              e.target.checked
+            )}/>
+        Show Timer
+      </label>
+      {showTimer && <Timer />}
+    </>
   )
 }
 
