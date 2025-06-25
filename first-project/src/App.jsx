32c9f9e4bf832a62904490c48569b7fa5c44df99
Son Seung Min <sonsmmm@gmail.com>
2025-06-25 14:05:31 +0900

    최적화

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,24 +1,37 @@
 import './App.css'
 import { useState } from 'react'
-import Timer from './Timer'
 
-const App = () => {
-  const [showTimer, setShowTimer]
-   = useState(false)
+function App() {
+  const [count1, setCount1] = useState(0)
+  const [count2, setCount2] = useState(0)
+
+  const heavyCalculation = (num) => {
+    console.log('Calculating...')
+    let result = 0
+    for (let i = 0; i < 1000000000; i++) {
+      result += num
+    }
+    return result
+  }
 
   return (
     <>
-      <label>
-        <input
-          type="checkbox"
-          checked={showTimer}
-          onChange={
-            (e) => setShowTimer(
-              e.target.checked
-            )}/>
-        Show Timer
-      </label>
-      {showTimer && <Timer />}
+      <p>Counter 1: {count1}</p>
+      <button onClick={
+        () => setCount1(count1 + 1)
+      }>+</button>
+      <button onClick={
+        () => setCount1(count1 - 1)
+      }>-</button>
+      <p>Counter 2: {
+        heavyCalculation(count2)
+      }</p>
+      <button onClick={
+        () => setCount2(count2 + 1)
+      }>+</button>
+      <button onClick={
+        () => setCount2(count2 - 1)
+      }>-</button>
     </>
   )
 }
