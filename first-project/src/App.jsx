23817ee22c7888fbc244c09f34fb4e1ba1bfd275
Son Seung Min <sonsmmm@gmail.com>
2025-06-25 14:55:03 +0900

    useRef

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,20 +1,22 @@
 import './App.css'
-import { CountProvider } from './contexts/CountContext'
-import { ToggleProvider } from './contexts/ToggleContext'
-import Child1 from './Child1'
-import Child3 from './Child3'
+import { useRef } from 'react'
 
-function App() {
+const App = () => {
+  const inputRef = useRef(null)
+
+  const handleFocus = () => {
+    console.log(inputRef.current)
+    inputRef.current.focus()
+  }
+
   return (
-    <>
-      <h2>App</h2>
-      <CountProvider>
-        <Child1 />
-      </CountProvider>
-      <ToggleProvider>
-        <Child3 />
-      </ToggleProvider>
-    </>
+    <div>
+      <input ref={inputRef}
+      type="text" placeholder='Type...'/>
+      <button onClick={handleFocus}>
+        Focus Input
+      </button>
+    </div>
   )
 }
 
