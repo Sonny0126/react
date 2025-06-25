7ffe21095367ef11a453f32a9fc6781387fa5083
Son Seung Min <sonsmmm@gmail.com>
2025-06-25 14:27:57 +0900

    최적화

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,39 +1,24 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
+import Child from './Child'
 
 function App() {
-  const [count1, setCount1] = useState(0)
-  const [count2, setCount2] = useState(0)
+  const [count, setCount] = useState(0)
+  const [active, setActive] = useState(true)
 
-  const heavyCalculation = (num) => {
-    console.log('Calculating...')
-    let result = 0
-    for (let i = 0; i < 1000000000; i++) {
-      result += num
-    }
-    return result
-  }
+  const handleClick = useCallback(
+    () => { setCount(c => c + 1) }, [])
 
   return (
     <>
-      <p>Counter 1: {count1}</p>
-      <button onClick={
-        () => setCount1(count1 + 1)
-      }>+</button>
-      <button onClick={
-        () => setCount1(count1 - 1)
-      }>-</button>
-      <p>Counter 2: {
-        heavyCalculation(count2)
-      }</p>
-      <button onClick={
-        () => setCount2(count2 + 1)
-      }>+</button>
-      <button onClick={
-        () => setCount2(count2 - 1)
-      }>-</button>
+      <h2>Parent</h2>
+      <button onClick={() => setActive(a => !a)}>
+        Toggle Active
+      </button>
+      <p>Count: {count}</p>
+      <Child active={active} 
+        onClick={handleClick} />
     </>
   )
 }
-
 export default App
